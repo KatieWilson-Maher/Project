7385c97457757f22bf4c4dceaa780ddb80418745
katie <katie@DESKTOP-CICGU7F>
2020-04-04 23:58:26 -0500

    Created more methods for vectors and matrices.

--- src/gradedexercise/NewClass.java
+++ src/gradedexercise/NewClass.java
@@ -15,19 +15,23 @@ class Matrix {
 
     private final double[][] elements;
 
+    //DONE
     public Matrix() {
         this.elements = new double[4][4];
         this.identity();
     }
 
+    //DONE
     public double get(int row, int column) {
         return this.elements[row][column];
     }
 
+    //DONE
     public void set(int row, int column, double value) {
         this.elements[row][column] = value;
     }
 
+    //DONE
     public final void identity() {
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
@@ -40,6 +44,7 @@ class Matrix {
         }
     }
 
+    //DONE
     private String rowToString(int row) {
         StringBuilder result = new StringBuilder();
         result.append("[ ");
@@ -52,16 +57,18 @@ class Matrix {
         return result.toString();
     }
 
+    //DONE
     @Override
     public String toString() {
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < 4; i++) {
             result.append(rowToString(i));
             result.append("\n");
-        } 
+        }
         return result.toString();
     }
 
+    //DONE
     public Matrix multiply(Matrix otherMatrix) {
         Matrix product = new Matrix();
         for (int row = 0; row < 4; row++) {
@@ -77,14 +84,37 @@ class Matrix {
         return product;
     }
 
-    public Matrix matrixRotationXAxis() {
-        return null;
+    // The rotateX() method has a single floating point parameter. Its return
+    // type is void. That parameter represents an angle. The method sets the 
+    // values of the elements of the matrix to create a matrix that models a
+    // rotation about the x-axis.
+    //DONE/TO TEST
+    public void rotateX(double angle) {
+        this.identity();
+        this.set(1, 1, Math.cos(angle));
+        this.set(1, 2, -Math.sin(angle));
+        this.set(2, 1, Math.sin(angle));
+        this.set(2, 2, Math.cos(angle));
     }
 
-    public Matrix matrixRotationYAxis() {
-        return null;
+    //The rotateY() method has a single floating point parameter. Its return
+    // type is void. That parameter represents an angle. The method sets the
+    // values of the elements of the matrix to create a matrix that models a
+    // rotation about the y-axis.
+    //DONE/TO TEST
+    public void rotateY(double angle) {
+        this.identity();
+        this.set(0, 0, Math.cos(angle));
+        this.set(0, 2, Math.sin(angle));
+        this.set(2, 0, -Math.sin(angle));
+        this.set(2, 2, Math.cos(angle));
     }
 
+    //The rotateZ() method has a single floating point parameter. Its return type
+    // is void. That parameter represents an angle. The method sets the values of
+    // the elements of the matrix to create a matrix that models a rotation about
+    // the z-axis
+    //DONE
     public void rotationZ(double angle) {
         this.identity();
         this.set(0, 0, Math.cos(angle));
@@ -93,125 +123,201 @@ class Matrix {
         this.set(1, 1, Math.cos(angle));
     }
 
-    public Matrix matrixScaling() {
-        return null;
+    //The scale() method has 3 floating point parameters. Its return type is
+    // void. The parameters specify the factor by which a geometric figure is to
+    // be enlarged (or shrunk) along the x, y, and z axes. The method sets the
+    // values of the elements of the matrix to create a matrix that models a scaling.
+    //DONE/TO TEST
+    public void scale(double xChange, double yChange, double zChange) {
+        this.identity();
+        System.out.println(this.get(0, 0));
+        this.set(0,0,xChange);
+        this.set(1,1,yChange);
+        this.set(2,2,zChange);
     }
 
-    public Matrix matrixRotation() {
-        return null;
+    //The translate() method has 3 floating point parameters. Its return type is
+    // void. The parameters specify the distance by which a geometric figure is
+    // to be translated (moved) along the x, y, and z axes. The method sets the
+    // values of the elements of the matrix to create a matrix that models a
+    // translation.
+    //DONE/TO TEST
+    public void translate(float xChange, float yChange, float zChange) {
+        this.identity();
+        this.set(3,0,xChange);
+        this.set(3,1,yChange);
+        this.set(3,2,zChange);
     }
 
+    //The multiply() method has a Vector parameter. Its return type is Vector.
+    // The method computes the product of this matrix and the parameter.
+    //DONE
+    public Vector multiply(Vector v) {
+        Vector newV = new Vector();
+        double row1 = 0;
+        int vec = 0;
+        for (int row = 0; row < 4; row++) {
+            vec += 1;
+            row1 = 0;
+            for (int column = 0; column < 4; column++) {
+                double currentM = this.get(row, column);
+                double currentV = v.get(vec - 1);
+                double newVal = currentM * currentV;
+                if (vec < 5) {
+                    row1 += newVal;
+                    newV.set(vec - 1, row1);
+                }
+            }
+        }
+        return newV;
+    }
 }
 
 class Vector {
-    private double x,y,z,h;
-    
-    // A constructor will have no parameters. It will produce a Vector whose elements all have a values equal to zero.
+
+    private double x, y, z, h;
+
+    // A constructor will have no parameters. It will produce a Vector whose
+    // elements all have a values equal to zero.
+    //DONE
     public Vector() {
         this.x = x;
         this.y = y;
         this.z = z;
         this.h = h;
     }
-    
+
     // A constructor will have 3 floating point values. It will produce a Vector
     // whose x, y, and z elements have values equal those given by the parameter.
     // The fourth (homogeneous) coordinate will have a value equal to one.
-    public Vector(double one, double two, double three){
+    //DONE
+    public Vector(double one, double two, double three) {
         x = one;
         y = two;
         z = three;
         h = 1;
     }
-    
-    //The dot() method will have one Vector parameter. Its return type is double. It will compute the dot product of this vector and the parameter.
-    public double dot(Vector v){
-        return this.x*v.get(0) + this.y*v.get(1) + this.z*v.get(2) + this.h*v.get(3);
+
+    //The dot() method will have one Vector parameter. Its return type is double.
+    // It will compute the dot product of this vector and the parameter.
+    //DONE
+    public double dot(Vector v) {
+        return this.x * v.get(0) + this.y * v.get(1) + this.z * v.get(2) + this.h * v.get(3);
     }
-            
-    //The magnitude() method will have no parameters. Its return type is double. It will compute the magnitude of this vector.
-    public double magnitude(){
-        return Math.abs(Math.sqrt(x*x + y*y + z*z + h*h));
+
+    //The magnitude() method will have no parameters. Its return type is double.
+    // It will compute the magnitude of this vector.
+    //DONE
+    public double magnitude() {
+        return Math.abs(Math.sqrt(x * x + y * y + z * z + h * h));
     }
-    
+
     //The normalize() method will have no parameters. Its return type is Vector.
     // It will produce a vector that has the same direction as this vector but a
     // magnitude (length) equal to one.
     //direction = inverse tan of y/x
-//    public Vector normalize(){
-//        double dir = Math.atan(y/x);
-//        double mag = 1;
-//        
-//        
-//    }
+    //DONE/TO TEST
+    public Vector normalize(){
+        Vector newV = new Vector();
+        double mag = this.magnitude();
+        newV.set(0,this.x/mag);
+        newV.set(1,this.y/mag);
+        newV.set(2,this.z/mag);
+        newV.set(3,this.h/mag);
+//      double dir = Math.atan(y/x);
+        return newV;
+    }
+    
     
     // The cross() method will have one Vector parameter. It return type is Vector.
     // It will compute the cross product of the x, y, and z components of this vector
     // and the x, y, and z components of the parameter.
-    
-    
 //    cx = aybz − azby
 //    cy = azbx − axbz
 //    cz = axby − aybx
     //x = 0, y = 1, z = 2
-    
-    public Vector cross(Vector v){
-        double cx = y*v.get(2) - z*v.get(1);
-        double cy = z*v.get(0) - x*v.get(2);
-        double cz = x*v.get(1) - y*v.get(0);
-        Vector c = new Vector(cx,cy,cz);
+    //DONE
+    public Vector cross(Vector v) {
+        double cx = y * v.get(2) - z * v.get(1);
+        double cy = z * v.get(0) - x * v.get(2);
+        double cz = x * v.get(1) - y * v.get(0);
+        Vector c = new Vector(cx, cy, cz);
         return c;
     }
-    
+
     //The get() method will have one integer parameter. It return type is double.
     // It will return the x component of the vector if the parameter's value equals 0,
     // the y component if the parameter's value equals 1, the z component if the parameter's
-    // value equals 2, and the homogeneous component if the parameter's value equals 3.        
+    // value equals 2, and the homogeneous component if the parameter's value equals 3.  
+    //DONE/COULD FIX
     public double get(int index) {
         double returnValue = 0;
-        if (index == 0){
+        if (index == 0) {
             returnValue = x;
         }
-        if (index == 1){
+        if (index == 1) {
             returnValue = y;
         }
-        if (index == 2){
+        if (index == 2) {
             returnValue = z;
         }
-        if (index == 3){
+        if (index == 3) {
             returnValue = h;
         }
         return returnValue;
     }
 
+    //DONE/COULD FIX
+    public void set(int index, double val) {
+        if (index == 0) {
+            this.x = val;
+        }
+        if (index == 1) {
+            this.y = val;
+        }
+        if (index == 2) {
+            this.z = val;
+        }
+        if (index == 3) {
+            this.h = val;
+        }
+    }
+
+    //DONE
     @Override
     public String toString() {
         return "(" + x + ", " + y + ", " + z + ", " + h + ")";
     }
-}
 
-class both {
-    // The multiply() method has a Vector parameter. Its return type is Vector.
-    // The method computes the product of this matrix and the parameter.
-    public Vector multiply(Vector v) {
-        return null;
-    }
 }
 
 public class NewClass {
 
     public static void main(String[] args) {
-        Vector u = new Vector(2,3,4);
-        Vector v = new Vector(5,6,7);
-        //System.out.println(u.cross(v));
+        //Vector u = new Vector(2, 3, 4);
+        Vector v = new Vector(1, 2, 3);
+        System.out.println(v.normalize());
+        //System.out.println(v.toString());
+        
 
         Matrix m = new Matrix();
-        m.set(0, 0, 5);
+
+        int i = 0;
+//        for (int row = 0; row < 4; row++) {
+//            for (int column = 0; column < 4; column++) {
+//                //System.out.println("row: "+row+" column: "+column);
+//                m.set(row, column, i);
+//                i += 1;
+//            }
+//        }
+
         //System.out.println(m);
         Matrix n = new Matrix();
+        n.translate(2, 4, 6);
+        //System.out.println(n.toString());
         //System.out.println(m.multiply(n));
         //System.out.println(m.identityMatrix());
-       // System.out.println(m.toString());
-
+        //System.out.println(m.toString());
+        //System.out.println(m.multiply(v));
     }
 }
